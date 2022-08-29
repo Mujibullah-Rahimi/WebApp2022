25f49b12c00233c54db550be0e9b9a29186ac68f
MujibullahRahimi <mujibbullah@hotmail.no>
2022-08-29 10:44:45 +0200

    CurrencyConverter finished

--- CurrencyConverter/index.js
+++ CurrencyConverter/index.js
@@ -1,26 +1,71 @@
-// Benytter en innebygget modul fra Node for å lese fra terminalen
-const readline = require("readline");
+const prompt = require('prompt-sync')()
 
-// Setter opp nødvendige konfigurasjon
-// Les fra kommandolinjen og skriv ut til kommandolinjen
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const rates = {
+  dkk: 1.34,
+  eur: 10,
+  usd: 8,
+};
 
-const language = "Node";
 const translations = {
-    no:{
-
+  no: {
+    welcomeMessage: "Norsk (no)",
+    valueMessage: "Hvilken verdi skal du skal konvertere? ",
+    currencies: {
+      usd: "Amerikanske dollar",
+      eur: "Euro",
+      dkk: "Danske kroner",
     },
-    en:{
-        
-    }
+  },
+  en: {
+    welcomeMessage: "English (en)",
+    valueMessage: "What value to convert? ",
+    currencies: {
+      usd: "American dollar",
+      eur: "Euro",
+      dkk: "Danish krone",
+    },
+  },
+};
+
+function getWelcomeMessage(translations) {
+  let message = "";
+  for (const language of Object.values(translations)) {
+    message += `${language["welcomeMessage"]} `;
+  }
+  return message;
 }
 
-var prompt = require('prompt-sync')();
-//
-// get input from the user.
-//
-var n = prompt('How many more times? ');
+function getValueMessage(translation) {
+  return translation["valueMessage"];
+}
 
+function handleConvertion(rate, value, decimals = 2) {
+  return Number(Number(value) / Number(rate)).toFixed(decimals);
+}
+
+function getCurrencyMessage(currencies) {
+  let message = "";
+
+  for (const entry of Object.entries(currencies)) {
+    console.log(entry)
+    const [short, long] = entry;
+    message += `${long}(${short}) `;
+  }
+
+  return message;
+}
+
+function start(translations, rates) {
+  const language = prompt(getWelcomeMessage(translations));
+  const translation = translations[language];
+  const currencies = translation["currencies"];
+  const valueMessage = getValueMessage(translation);
+  const value = prompt(valueMessage);
+  const currencyMessage = getCurrencyMessage(currencies);
+  const currency = prompt(currencyMessage);
+  return handleConvertion(rates[currency], value);
+}
+
+const result = start(translations, rates);
+
+console.log(result);
